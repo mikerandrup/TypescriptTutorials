5a96ccc1e00275648588bb89752b71f33ab33ba7
Mike Randrup <github@mikerandrup.com>
2014-12-10 14:42:34 -0600

    Added experiment with TypeLITE C# -> TypeScript model converter.

---
+++ MvcApplication1/Models/SampleModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TypeLite;
+
+namespace MvcApplication1.Models
+{
+    [TsClass]
+    public class SampleModel
+    {
+        public SampleModel(string name)
+        {
+            _name = name;
+        }
+
+        private string _name;
+        public string Name { get { return _name; } }
+
+        public int PrimaryKey { get; set; }
+
+        public float Price { get; set; }
+
+
+
+    }
+}
